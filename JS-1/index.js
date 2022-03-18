2ecb6852c8bb824ba8ef39ff995f36e15ca420dd
AxelAngelucci <82065422+AxelAngelucci@users.noreply.github.com>
2022-03-18 11:36:01 -0300

    Task one of module three

---
+++ JS-1/index.js
@@ -0,0 +1,6 @@
+const name = prompt('Como te llamas?');
+const age  = prompt('Edad?');
+const activity = prompt('Cual es tu actividad favorita?');
+const skills   = prompt('Que lenguaje de programación conoces?');
+
+alert(`Hola ${name}! tu edad es de ${age} años, Tu actividad favorita es ${activity} y conoces un poco sobre ${skills}, ¿Correcto?`);
